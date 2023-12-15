758729b945c3c3f48f9dcbedbd4bd72c97e279f7
Tadeáš Kozub <xkozub1@fi.muni.cz>
2023-12-15 15:47:21 +0100

    add partial register validation, error snack msgs and submit on enter

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -7,10 +7,61 @@ import 'package:go_router/go_router.dart';
 
 import 'firebase_auth_services.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController _controllerUsername = TextEditingController();
   final TextEditingController _controllerEmail = TextEditingController();
   final TextEditingController _controllerPassword = TextEditingController();
+
+  Timer? _debounce;
+  bool? isUsernameAvailable;
+  String errorMessage = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _controllerUsername.addListener(_onUsernameChanged);
+  }
+
+  @override
+  void dispose() {
+    _controllerUsername.dispose();
+    _debounce?.cancel();
+    super.dispose();
+  }
+
+  _onUsernameChanged() {
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(const Duration(milliseconds: 500), () {
+      checkUsernameAvailability(_controllerUsername.text);
+    });
+  }
+
+  void checkUsernameAvailability(String username) async {
+    if (_controllerUsername.text.isEmpty) {
+      setState(() {
+        isUsernameAvailable = false;
+        errorMessage = "Invalid username";
+      });
+      return;
+    }
+    final querySnapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .where('name', isEqualTo: username)
+        .limit(1)
+        .get();
+
+    setState(() {
+      isUsernameAvailable = querySnapshot.docs.isEmpty;
+      errorMessage = "Name already taken";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +85,25 @@ class RegisterScreen extends StatelessWidget {
           .catchError((error) => print("Failed to add user: $error"));
     }
 
+    void register() async {
+      try {
+        // don't you dare forget await here or anywhere else 💀💀💀
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: _controllerEmail.text,
+          password: _controllerPassword.text,
+        );
+      } catch (e) {
+        print("error registering the user: $e");
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text("$e"),
+        ));
+        return;
+      }
+      await addUser(
+          _controllerUsername.text, _controllerEmail.text, "no bio available");
+      context.go('/login');
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Register'),
@@ -41,6 +111,27 @@ class RegisterScreen extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Username',
+                    labelText: 'User name',
+                    errorText:
+                        isUsernameAvailable == false ? errorMessage : null,
+                    suffixIcon: isUsernameAvailable == null
+                        ? null
+                        : isUsernameAvailable!
+                            ? Icon(Icons.check_circle, color: Colors.green)
+                            : Icon(Icons.error, color: Colors.red),
+                  ),
+                  controller: _controllerUsername,
+                ),
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
@@ -60,6 +151,7 @@ class RegisterScreen extends StatelessWidget {
                   decoration: InputDecoration(
                       border: OutlineInputBorder(), hintText: 'Password'),
                   controller: _controllerPassword,
+                  onSubmitted: (_) => {register()},
                 ),
               ),
             ),
@@ -67,14 +159,7 @@ class RegisterScreen extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: ElevatedButton(
                   onPressed: () async {
-                    if (await Auth().registerUser(
-                            email: _controllerEmail.text,
-                            password: _controllerPassword.text) ==
-                        true) {
-                      await addUser("placeholder_nickname",
-                          _controllerEmail.text, "ahoj :)");
-                      context.go('/login');
-                    }
+                    register();
                   },
                   child: Text('register')),
             ),
