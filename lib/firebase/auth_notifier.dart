27740750f3a1d0338c64cfbd9200964f6e294aef
Tadeáš Kozub <xkozub1@fi.muni.cz>
2024-01-13 19:38:41 +0100

    remove comments

--- lib/firebase/auth_notifier.dart
+++ lib/firebase/auth_notifier.dart
@@ -1,12 +1,6 @@
-// ignore_for_file: unused_import
-
 import 'package:flutter/cupertino.dart';
-import 'package:guitar_app/firebase/firebase_auth_services.dart';
 
 class AuthController with ChangeNotifier {
-//Within this section, you can integrate authentication methods
-//such as Firebase, SharedPreferences, and more.
-
   bool isLoggedIn = false;
 
   void signIn() {
